d249bf028dcdf6a944c4a4fc415e1665f1977cbb
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-04-20 03:46:36 +0900

    Updates

---
+++ 0005switch.js
@@ -0,0 +1,28 @@
+// const a = 1;
+
+// switch (a) {
+//     case 1:
+//         console.log('1')
+//         break
+//     case 10:
+//         console.log('10')
+//         break
+//     case 100:
+//         console.log('100')
+//         break
+//     default:
+//         console.log('I don\'t know')
+//         break
+// }
+
+const a = 1;
+
+if (a == 1) {
+  console.log("1");
+} else if (a == 10) {
+  console.log("10");
+} else if (a == 100) {
+  console.log("100");
+} else {
+  console.log("I don't know");
+}
---
+++ 0006while.js
@@ -0,0 +1,37 @@
+// let n = 0
+// while (n <= 5) {
+//     console.log(n);
+//     n++
+// }
+
+// for (let a = 0; a <= 5; a++) {
+//     console.log(a)
+// }
+
+// const n = 7;
+// let result = 1;
+
+// for (let count = 1; count <= n; count++) {
+//     result = result * count;
+// }
+
+// console.log(result)
+
+// const star = '*';
+// let num = 5;
+
+// for (let floor = 1; floor <= num; floor++) {
+//     console.log(' '.repeat(num - floor) + star.repeat(floor))
+// }
+
+
+// const num = 9;
+// for (let count = 1; count <= 100 / num; count++) {
+//     console.log(count * num)
+// }
+
+
+for (let count = 1; count <= 6; count++) {
+    let n = Math.floor(Math.random() * 45) + 1;
+    console.log(n)
+}
